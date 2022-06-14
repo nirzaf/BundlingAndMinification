d231e6bcca00ebb02b85b7709a89df985e5e9c48
nirzaf <mfmfazrin1986@gmail.com>
2022-06-14 23:11:26 +0530

    loggerFactory.AddConsole(Configuration.GetSection("Logging"));             loggerFactory.AddDebug();

--- BundlingAndMinification/Controllers/HomeController.cs
+++ BundlingAndMinification/Controllers/HomeController.cs
@@ -12,14 +12,12 @@ namespace BundlingAndMinification.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
--- BundlingAndMinification/Startup.cs
+++ BundlingAndMinification/Startup.cs
@@ -30,9 +30,6 @@ namespace BundlingAndMinification
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -45,7 +42,7 @@ namespace BundlingAndMinification
 
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
+            app?.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
